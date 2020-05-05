c0d3f860481f581a52337d6b6013765d89b7a0ad
MASTERGUY <murtipipo@gmail.com>
2020-05-05 18:57:02 +0530

    Revert "msm8953-common: Reimplement FM Radio"
    This reverts commit 68d51e8818f58809f768809b882086a6ee3c7090.
    it is fcked up for us

--- msm8953.mk
+++ msm8953.mk
@@ -162,12 +162,8 @@ PRODUCT_PACKAGES += \
 
 # FM
 PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
-
-PRODUCT_BOOT_JARS += \
-    qcom.fmradio
+    FMRadio \
+    libfmjni
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
