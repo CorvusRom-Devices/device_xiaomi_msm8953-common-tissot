ec76d12ebb9e5773b6c2ad769bbff88807842b8d
mountaser <mountaser1998@gmail.com>
2020-06-01 05:31:51 +0000

    msm8953-common: Added Built-in Spectrum Support
    This reverts commit 4c55aeedf4fa6d368143e4ca7487dff9df0d1ff1.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -90,3 +90,10 @@ LOCAL_SRC_FILES    := etc/move_wifi_data.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.spectrum.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.spectrum.rc
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
