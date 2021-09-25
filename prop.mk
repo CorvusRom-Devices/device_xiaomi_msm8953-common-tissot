758422f866a64e17f5dd05814c14e30b4873043c
KenHV <yo@kenharris.xyz>
2021-09-25 12:52:30 +0000

    msm8953-common: Introduce memory management tweaks
    Signed-off-by: ajitlenka30 <ajitlenka30@gmail.com>

--- prop.mk
+++ prop.mk
@@ -181,6 +181,18 @@ ro.telephony.call_ring.multiple=false \
 ro.telephony.default_network=22,20 \
 service.qti.ims.enabled=1
 
+# Memory Management
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.config.fha_enable=true \
+ro.sys.fw.bg_apps_limit=32 \
+ro.config.dha_cached_max=16 \
+ro.config.dha_empty_max=42 \
+ro.config.dha_empty_init=32 \
+ro.config.dha_lmk_scale=0.545 \
+ro.config.dha_th_rate=2.3 \
+ro.config.sdha_apps_bg_max=64 \
+ro.config.sdha_apps_bg_min=8 \
+
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 debug.sf.early_app_phase_offset_ns=1500000 \
